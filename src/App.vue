b37939d57649e8c16deb3e3d6aea3569f9072906
Andrey Kolesin <kolesinandrei1910@gmail.com>
2022-11-22 12:05:48 +0300

    rename page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MenuPage msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MenuPage from './components/MenuPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MenuPage
   }
 }
 </script>
